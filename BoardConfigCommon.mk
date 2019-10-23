440e53b4564284853ef18bfb93c3243df203c8cc
bikram557 <kitkatmukherjee2015@gmail.com>
2019-10-23 14:52:18 +0000

    Santoni: Preparing DerpFest Q Tree

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -221,7 +221,7 @@ TARGET_RECOVERY_FSTAB 		 := $(VENDOR_PATH)/rootdir/fstab.qcom
 # SELinux
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 SELINUX_IGNORE_NEVERALLOWS := true
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wi-Fi
