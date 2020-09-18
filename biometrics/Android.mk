0b49ff3eb385b7bda0a9a6437b0dcb31ad47ba4f
Frank Eggen <eggen@emden.net>
2020-09-18 18:01:08 +0200

    Remove libhwbinder/libhidltransport deps

--- biometrics/Android.mk
+++ biometrics/Android.mk
@@ -14,10 +14,8 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils \
     liblog \
     libhidlbase \
-    libhidltransport \
     libhardware \
     libutils \
-    libhwbinder \
     android.hardware.biometrics.fingerprint@2.1
 
 LOCAL_CFLAGS += -DUSE_FINGERPRINT_2_0
@@ -38,10 +36,8 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils \
     liblog \
     libhidlbase \
-    libhidltransport \
     libhardware \
     libutils \
-    libhwbinder \
     android.hardware.biometrics.fingerprint@2.1
 
 include $(BUILD_EXECUTABLE)
